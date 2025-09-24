202a6a0c381b4660beedad96d6f354e47ab97dd8
Thibault Pouch <thibault.pouch@epitech.eu>
2025-09-24 09:02:39 +0200

    refactor(client): replace main function with App class for improved structure

--- src/client/src/main.cpp
+++ src/client/src/main.cpp
@@ -1,30 +1,8 @@
 #include <raylib.h>
-#include <cmath>
+#include "../include/client/ui/App.hpp"
 
 int main() {
-    const int screenWidth = 960;
-    const int screenHeight = 540;
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "R-Type Client (raylib)");
-    SetTargetFPS(60);
-
-    float t = 0.f;
-    while (!WindowShouldClose()) {
-        float dt = GetFrameTime();
-        t += dt;
-
-        BeginDrawing();
-        ClearBackground(BLACK);
-        // very simple starfield: moving dots
-        for (int i = 0; i < 200; ++i) {
-            float x = fmodf((i * 73 + t * 60), (float)GetScreenWidth());
-            float y = (i * 37) % GetScreenHeight();
-            DrawPixel((int)x, (int)y, (i % 7 == 0) ? RAYWHITE : DARKGRAY);
-        }
-        DrawText("R-Type prototype - raylib client", 20, 20, 20, RAYWHITE);
-        EndDrawing();
-    }
-
-    CloseWindow();
+    client::ui::App app;
+    app.run();
     return 0;
 }
