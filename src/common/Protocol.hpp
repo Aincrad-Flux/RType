126a10e749a8b55dd1db76aabe2885cc0f1a66a9
Thibault Pouch <thibault.pouch@epitech.eu>
2025-10-05 12:48:00 +0200

    refactor: simplify Protocol.hpp by removing unused definitions

--- src/common/Protocol.hpp
+++ src/common/Protocol.hpp
@@ -1,27 +1,3 @@
+// Wrapper header to expose the canonical public protocol definitions
 #pragma once
-#include <cstdint>
-#include <array>
-
-namespace rtype::net {
-
-enum class MsgType : std::uint8_t {
-    Hello = 1,
-    HelloAck,
-    Input,
-    State,
-    Spawn,
-    Despawn,
-    Ping,
-    Pong
-};
-
-struct Header {
-    std::uint16_t size;   // payload size excluding header
-    MsgType type;
-    std::uint8_t version;
-};
-
-static constexpr std::uint8_t ProtocolVersion = 1;
-static constexpr std::size_t HeaderSize = sizeof(Header);
-
-}
+#include "include/common/Protocol.hpp"
